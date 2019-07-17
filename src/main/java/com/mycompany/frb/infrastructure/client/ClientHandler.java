5c233e2edc982ec8c5b8ecbd95717b5186969960
Colin But <colinbut@users.noreply.github.com>
2019-07-18 00:04:20 +0100

    placeholder source files, needs worked on

---
+++ src/main/java/com/mycompany/frb/infrastructure/client/ClientHandler.java
@@ -0,0 +1,11 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.frb.infrastructure.client;
+
+public class ClientHandler {
+
+
+}
